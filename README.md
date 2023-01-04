668a154381476a0c95640c692c8ce12028054ca7
2sd17cs126 <56037040+2sd17cs126@users.noreply.github.com>
2023-01-04 12:33:08 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Done_with_orthogonal_array_till_UI
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ihvedl-wifwwd)
---
+++ main.ts
@@ -0,0 +1,37 @@
+import './polyfills';
+import { ModalModule } from './app/_modal/modal.module'
+import {HttpClientModule} from '@angular/common/http';
+import {NgModule} from '@angular/core';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatNativeDateModule} from '@angular/material';
+import {BrowserModule} from '@angular/platform-browser';
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {DemoMaterialModule} from './material-module';
+
+import {ExpansionOverviewExample} from './app/expansion-overview-example';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule,
+    DemoMaterialModule,
+    MatNativeDateModule,
+    ReactiveFormsModule,
+    ModalModule,
+  ],
+  entryComponents: [ExpansionOverviewExample],
+  declarations: [ExpansionOverviewExample],
+  bootstrap: [ExpansionOverviewExample],
+  providers: []
+})
+export class AppModule {}
+
+platformBrowserDynamic().bootstrapModule(AppModule);
+
+
+/**  Copyright 2018 Google Inc. All Rights Reserved.
+    Use of this source code is governed by an MIT-style license that
+    can be found in the LICENSE file at http://angular.io/license */
